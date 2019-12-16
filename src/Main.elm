bb5ae35e2e44735a334c76015ddc0dcad84c0f20
Olof Bjarnason <olof.bjarnason@gmail.com>
2019-12-16 19:54:12 +0100

    use elm ui for layout

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,51 @@
-module Main exposing (main)
+module Main exposing (main, myElement, myRowOfStuff)
 
-import Html exposing (text)
+import Element
+    exposing
+        ( Element
+        , alignRight
+        , centerX
+        , centerY
+        , el
+        , fill
+        , padding
+        , rgb255
+        , row
+        , spacing
+        , text
+        , width
+        )
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 
 
 main =
-    text "Hello -- world!"
+    Element.layout []
+        myRowOfStuff
+
+
+myRowOfStuff : Element msg
+myRowOfStuff =
+    row [ width fill, centerY, spacing 30 ]
+        [ el [ centerX ] myElement
+        ]
+
+
+myElement : Element msg
+myElement =
+    el
+        [ bg
+        , fg
+        , Border.rounded 3
+        , padding 30
+        ]
+        (text "Hello world")
+
+
+bg =
+    Background.color (rgb255 0 140 165)
+
+
+fg =
+    Font.color (rgb255 255 255 255)
