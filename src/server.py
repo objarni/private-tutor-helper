fe5460b3d2df72fe749a2b1e60be78eeb4e81e27
Olof Bjarnason <olof.bjarnason@gmail.com>
2019-12-16 20:11:34 +0100

    add json loading api and serve html files statically

--- src/server.py
+++ src/server.py
@@ -1,10 +1,21 @@
 # coding: utf-8
+from bottle import route, run, template, static_file
 
-from bottle import route, run, template
+
+@route('/js/app.js')
+def elm_app():
+    return static_file('js/app.js', root='.')
+
 
 @route('/')
 def index():
-    return '<b>Hello world</b>!'
+    return static_file('index.html', root='.')
+
+
+@route('/load.json')
+def load_json():
+    return static_file('journal.json', root='.')
+
 
 run(host='localhost', port=8000)
 
