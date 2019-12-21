c662d9b6609be48bf040ce843212b4b0bf1e3c36
Olof Bjarnason <olof.bjarnason@gmail.com>
2019-12-22 00:18:45 +0100

    let black reformat server.py

--- src/server.py
+++ src/server.py
@@ -2,15 +2,19 @@
 import bottle
 
 
-static_paths = ["js/app.js", "index.html", "journal.json"]
+static_paths = [
+    "js/app.js",
+    "index.html",
+    "journal.json",
+]
 
 
 @bottle.route("/<path:re:.*>")
 def path(path):
     if path in static_paths:
-    	response = bottle.static_file(path, ".")
-    	response.set_header('Cache-Control', 'public, max-age=5')
-    	return response
+        response = bottle.static_file(path, ".")
+        response.set_header("Cache-Control", "public, max-age=5")
+        return response
     else:
         return "Unknown path"
 
