e84d168270afbbf1770b7f9d1ed48a66933eb21d
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-02-14 10:13:48 +0100

    switch to Shake for building

---
+++ Build.hs
@@ -0,0 +1,33 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-14.25
+
+import Development.Shake
+import Development.Shake.FilePath
+
+main = shakeArgs shakeOptions $ do
+    want ["output/server.py"
+         ,"output/bottle.py"
+         ,"output/index.html"
+         ,"output/js/app.js"]
+
+    "output/*.py" %> \out -> do
+        let src = replaceDirectory1 out "src"
+        need [src]
+        cmd "cp" [src] "output"
+
+    "output/index.html" %> \out -> do
+        need ["src/index.html"]
+        cmd "cp src/index.html output"
+
+    "output/js/app.js" %> \out -> do
+        need ["src/Main.elm", "src/Pupil.elm"]
+        cmd "elm make src/Main.elm" ["--output=" ++ out]
+
+    phony "clean" $ do
+        -- cmd "rm -rf output/"
+        let toRemove = [".shake/", 
+                        "elm-stuff/"]
+        cmd "rm -rf" toRemove
+
+-- output/js/app.js: src/*.elm
+--     elm make src/Main.elm --output=output/js/app.js
--- Makefile
+++ Makefile
@@ -2,30 +2,6 @@
 run:
 	cd output && python3.6 server.py
 
-output:
-	mkdir output
-
-PHONY: build
-build: output output/index.html output/js/app.js output/bottle.py output/server.py
-	echo Building...
-
-PHONY: lci
+.PHONY: lci
 lci:
-	ls src/* | entr sh -c 'clear && make build'
-
-PHONY: clean
-clean:
-	$(RM) output/
-	$(RM) elm-stuff/
-
-output/js/app.js: src/*.elm
-	elm make src/Main.elm --output=output/js/app.js
-
-output/index.html: src/index.html
-	cp src/index.html output
-
-output/bottle.py: src/bottle.py
-	cp src/bottle.py output
-
-output/server.py: src/server.py
-	cp src/server.py output
+	ls src/* | entr sh -c 'clear && ./Build.hs'
